de5dd26d9d5c1e73da34614e4f7db1c7d5c63298
2021-DEV1-113 <2021-DEV1-113@localhost>
2021-11-23 10:59:03 +0100

    Gameplay: add on square click function

--- src/component/Game.jsx
+++ src/component/Game.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 
 import Board from "./Board";
+import { calculateWinner } from './../helpers/logicHelpers';
 
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
-  const onSquareClick = () => {};
+  const [xPlaysNext, setXPlaysNext] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onSquareClick = (index) => {
+    const boardClone = [...board];
+    if(calculateWinner(board)){
+      setErrorMessage('there is already a winner');
+      return;
+    } else if(boardClone[index]){
+      setErrorMessage('the square is already occupied');
+      return
+    } else {
+      boardClone[index] = xPlaysNext ? 'x' : 'o';
+      setBoard(boardClone);
+      setXPlaysNext(!xPlaysNext);
+    }
+  };
+
   return (
     <div>
       Tic tac toe
